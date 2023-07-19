1b0bec2774206513c737552755f38f021ea15b5e
LokeshJawale1996 <jawalelokesh2411@gmail.com>
2023-07-19 11:05:47 +0530

    initial commit for data persistence with api request

---
+++ books_api/src/App.js
@@ -0,0 +1,40 @@
+import "./App.css";
+import { useState } from "react";
+import BookCreate from "./components/BookCreate";
+import BookList from './components/BookList'
+function App() {
+  const [books, setBooks] = useState([]);
+
+  const editBookById = (id,newTitle)=> {
+    const updatedBooks = books.map((book) => {
+      if(book.id===id){
+        return {...book,title:newTitle};
+      }
+      return book;
+    })
+    setBooks(updatedBooks);
+  }
+  const deleteBookById = (id) => {
+    const updatedBooks = books.filter((book) => {
+      return book.id !== id;
+    })
+    setBooks(updatedBooks);
+  }
+  const createBook = (title) => {
+    // books.push({id:123,title:title});
+    console.log("Need to add book with:", title);
+    const updatedBooks = [...books, { id: Math.round(Math.random() *9999), title }];
+    setBooks(updatedBooks);
+    console.log(books);
+  };
+
+  return (
+    <div className="app">
+      <h1>Reading List</h1>
+      <BookList books={books} onDelete={deleteBookById} onEdit={editBookById}/>
+      <BookCreate onCreate={createBook} />
+    </div>
+  );
+}
+
+export default App;
